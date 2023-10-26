875d485574255cc104d2c425b488622a93efca94
Ananias123 <omarmudacumura7@gmail.com>
2023-10-25 17:51:15 -0700

    Task 000 recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,15 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++
+	_puts_recursion(s);
+
 }
-else
-_putchar('\n');
-}
+
