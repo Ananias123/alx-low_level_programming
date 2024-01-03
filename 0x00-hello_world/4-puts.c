1c403cd26a49af4293def5df979f5437ab3b6414
Ananias123 <omarmudacumura7@gmail.com>
2024-01-03 14:31:54 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The function that prints the word
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
