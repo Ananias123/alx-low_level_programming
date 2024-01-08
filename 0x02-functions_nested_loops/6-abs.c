78ef9b9584e00cc63ed8a7f78bd4d3078e0d2d05
Ananias123 <omarmudacumura7@gmail.com>
2024-01-08 22:00:50 +0200

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - a function that prints the absolute value of a number
+ * @k: the number to be checked
+ * Return: k
+ */
+int _abs(int k)
+{
+	if (k < 0)
+		return (-k);
+	else
+		return (k);
+}
+
+
